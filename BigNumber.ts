5a2f374b313679791429c0b181e72e76c6c7b08d
alfu32 <ioan.alferaru@gmail.com>
2020-06-02 11:23:37 +0200

    feature: add operator

--- BigNumber.ts
+++ BigNumber.ts
@@ -6,7 +6,6 @@ export class BigNumber{
     n.numbers = [...this.numbers];
     return n;
   }
-  https://www.acceptance.cc.cec/archibus/schema/ab-core/views/process-navigator/navigator-details.axvw?ticket=ST-25468630-jo84fYsL3tWwHsbzqYnOA1QNXN6sho4F4rHNzkp5ngKi4C7U2BDVPuCm6XWsE4SU54Is1mLbhXJn9jAPjgf3Fq-jpJZscgsw0KzTP3jULmpbl-ttq4cvlIrCY3E1rTCpzmWomqPapbl4bhzdtjn1lznOIwZakeipp1YjSwNvr3LWzY1eLUG7QBCIHaIlOUPhPZsaa
   public assign(value: number){
     let v = value;
     const b = this.base;
@@ -23,12 +22,32 @@ export class BigNumber{
     }
   }
   public add( n: number) {
-    let a = this;
-    let b = n;
-    let c = new BigNumber(0);
-
-    while( true ) {
-
+    let a = this.numbers;
+    let result = new BigNumber(n);
+    let b = result.numbers;
+    result = new BigNumber(n);
+    let carry = 0;
+    let base = this.base;
+    let rest = 0;
+    let sum = 0;
+    let index = 0;
+    while( rest !== 0 || carry !== 0 || a[index] !== undefined || b[index] !== undefined ) {
+      if( a[index] ) {
+        if( b[index] ) {
+          sum = a[index] + b[index] + carry;
+        } else {
+          sum = a[index] + carry;
+        } 
+      } else {
+        if( b[index] ) {
+          sum = b[index] + carry;
+        } else {
+          sum = carry;
+        }
+      }
+      rest = sum % base;
+      carry = ( sum - rest ) / base;
+      result[index] = rest;
     }
   }
 }
