23e262f640ce9b19bd6dcbbc730c33b076a72656
alfu32 <ioan.alferaru@gmail.com>
2020-06-02 13:45:38 +0200

    2 files modified

--- BigNumber.ts
+++ BigNumber.ts
@@ -1,6 +1,8 @@
+export const BIG_NUMBER_BASE = 1000;
 export class BigNumber{
-  public base = 65536;
+  public base = BIG_NUMBER_BASE;
   public numbers: Array<number> = new Array<number>();
+  intermediate: Array<any> = [];
   public static clone(): BigNumber{
     const n = new BigNumber(0);
     n.numbers = [...this.numbers];
@@ -8,93 +10,119 @@ export class BigNumber{
   }
   public assign(value: number){
     let v = value;
-    const b = this.base;
+    const b = BIG_NUMBER_BASE;
     this.numbers = [];
     while ( v >= b ) {
-      this.numbers.unshift(v % b);
+      this.numbers.push(v % b);
       v = (v - (v % b)) / b;
     }
-    this.numbers.unshift(v);
+    this.numbers.push(v);
   }
   public constructor(n?: number){
     if( n !== undefined) {
       this.assign(n);
     }
   }
-  public add( n: number) {
+  public add( n: number): BigNumber {
     let a = this.numbers;
+    let b = new BigNumber(n).numbers;
     let result = new BigNumber(n);
-    let b = result.numbers;
-    result = new BigNumber(n);
     let carry = 0;
-    let base = this.base;
+    let base = BIG_NUMBER_BASE;
     let rest = 0;
     let sum = 0;
     let index = 0;
     while (
-        rest !== 0
-        || carry !== 0
-        || a[index] !== undefined
-        || b[index] !== undefined
+        (
+          carry !== 0
+          || a[index] !== undefined
+          || b[index] !== undefined
+        )
       ) {
-      if( a[index] ) {
-        if( b[index] ) {
-          sum = a[index] + b[index] + carry;
-        } else {
-          sum = a[index] + carry;
-        } 
-      } else {
-        if( b[index] ) {
-          sum = b[index] + carry;
-        } else {
-          sum = carry;
-        }
-      }
+      sum = ((a[index]!==undefined)?a[index]:0)
+        + ((b[index]!==undefined)?b[index]:0)
+        + carry;
+      // if( a[index] ) {
+      //   if( b[index] ) {
+      //     sum = a[index] + b[index] + carry;
+      //   } else {
+      //     sum = a[index] + carry;
+      //   } 
+      // } else {
+      //   if( b[index] ) {
+      //     sum = b[index] + carry;
+      //   } else {
+      //     sum = carry;
+      //   }
+      // }
+      let expr = `${
+        (a[index]!==undefined)?a[index]:0
+      } + ${
+        (b[index]!==undefined)?b[index]:0
+      } + ${carry}`;
       rest = sum % base;
       carry = ( sum - rest ) / base;
-      result[index] = rest;
+      result.numbers[index] = rest;
+      result.intermediate.push({
+        sum, rest, carry,
+        expr
+      })
       index++;
     }
-    if( carry != 0) {
-      result[index] = carry;
-    }
+    // if(carry != 0) {
+    //   result.numbers[index] = carry;
+    // }
+    return result;
   }
-  public mul( n: number) {
+  shift( n: number): this {
+    const arr = new Array<number>(n).fill(0);
+    this.numbers.unshift( ...arr);
+    return this;
+  }
+  shiftRight( n: number): this {
+    while(n--)this.numbers.shift();
+    return this;
+  }
+  public mul( n: number): BigNumber {
     let a = this.numbers;
+    let b = new BigNumber(n).numbers;
     let result = new BigNumber(n);
-    let b = result.numbers;
-    result = new BigNumber(n);
     let carry = 0;
-    let base = this.base;
+    let base = BIG_NUMBER_BASE;
     let rest = 0;
-    let sum = 0;
+    let prod = 0;
     let index = 0;
     while(
-      rest !== 0
-      || carry !== 0
-      || a[index] !== undefined
-      || b[index] !== undefined
+        (
+          carry !== 0
+          || a[index] !== undefined
+          || b[index] !== undefined
+        )
     ) {
+      // prod = ((a[index]!==undefined)?a[index]:1)
+      //   * ((b[index]!==undefined)?b[index]:1)
+      //   + carry;
       if( a[index] ) {
         if( b[index] ) {
-          sum = a[index] * b[index] + carry;
+          prod = a[index] * b[index] + carry;
         } else {
-          sum = a[index] + carry;
+          prod = a[index] + carry;
         } 
       } else {
         if( b[index] ) {
-          sum = b[index] + carry;
+          prod = b[index] + carry;
         } else {
-          sum = carry;
+          prod = carry;
         }
       }
-      rest = sum % base;
-      carry = ( sum - rest ) / base;
-      result[index] = rest;
+      rest = prod % base;
+      carry = ( prod - rest ) / base;
+      result.numbers[index] = rest;
       index++;
     }
     if( carry != 0) {
-      result[index] = carry;
+      result.numbers[index] = carry;
     }
+    return result;
   }
 }
--- index.ts
+++ index.ts
@@ -2,8 +2,33 @@
 import './style.css';
 import { BigNumber } from './BigNumber';
 
+let tests = [
+  () => {
+    let result = new BigNumber(555666777888);
+  result = result.add(444333222112);
+    return result;
+  },
+  () => {
+    let result = new BigNumber(8999999);
+    result = result.add(111);
+    return result;
+  },
+  () => {
+    let result = new BigNumber(1000);
+    result = result.shift(2);
+    return result;
+  },
+  () => {
+    let result = new BigNumber(1201999111);
+    result = result.shiftRight(2);
+    return result;
+  }
+]
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-const a = new BigNumber(655565665);
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>
-<pre>${JSON.stringify(a).toString()}</pre>`;
+<pre>${
+  tests
+  .map( t => JSON.stringify(t(), null, '') )
+  .join('<br>')
+}</pre>`;
